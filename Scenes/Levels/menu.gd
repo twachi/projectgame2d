de77c29fa298f63126088fb4c94eefa184f3ca23
Wachirawut Thamviset <twachikku@gmail.com>
2025-08-19 08:47:04 +0700

    เพิ่ม กระสือ

--- Scenes/Levels/menu.gd
+++ Scenes/Levels/menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var pos = Vector2(0,0) 
-var speed = Vector2(-3,0)
+var speed = Vector2(-1,0)
 @onready var rig: Node2D = $CanvasLayer/Rig
 var x1=150
 var x2=1000
@@ -15,14 +15,15 @@ func _ready() -> void:
 	rig.get_node("AnimationPlayer").play("walk")
 	pos = rig.position
 	x2 = pos.x
+	rig.scale = Vector2(1.5,1.5)
 
 func _process(delta: float) -> void:
 	pos += speed
 	if pos.x < x1 : 
-		speed.x = 3
+		speed.x = 1
 		rig.scale.x=-1.5
 	if pos.x > x2 : 
-		speed.x = -3
+		speed.x = -1
 		rig.scale.x=1.5
 	rig.position = pos
 	
--- Scenes/Prefabs/monster_rig.gd
+++ Scenes/Prefabs/monster_rig.gd
@@ -5,3 +5,6 @@ extends Node2D
 func _on_attack_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		GameManager.player_damage(atk_power)
+
+func _ready() -> void:
+	$AttackArea/CollisionShape2D/Effect.emitting = true
